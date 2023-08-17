e5c959d42629b963dd8e4b779d80249ca8651f19
ArtemFedorchuk <fedorchuk.zver@gmail.com>
2023-08-17 23:19:18 +0300

    New webpack improvements

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import 'styles/styles.css'
 import 'styles/main.scss'
 import json from 'assets/json.json'
 
+class Util {
+	static date = Date.now();
+}
+
+console.log('date', Util.date)
+
 const post = new Post('Post');
 console.log('post', post.toString())
 console.log('json', json)
